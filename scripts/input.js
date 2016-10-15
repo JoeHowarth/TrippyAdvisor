930d949c074aed63957b62ede426009443f6e860
JoeHowarth <josephehowarth@gmail.com>
2016-10-15 04:21:14 -0400

    Update input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,6 +1,21 @@
 // centers map on input address
 // returns long lat 
-    
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: {lat: -34.397, lng: 150.644} //change to tufts 
+        });
+        var geocoder = new google.maps.Geocoder();
+        document.getElementById('submit').addEventListener('click', function() {
+          getLoc(geocoder, map);
+        
+        document.getElementById('submit').addEventListener('click', function() {
+          getPin(, map);
+        
+        daniel's alg(location
+        });
+      }
+
     function getLoc(geocoder, resultsMap) {
         var address = document.getElementById('address').value;
         geocoder.geocode({'address': address}, function(results, status) {
