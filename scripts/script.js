fb2bccd9b7f6ccba88af4bb30c1bdb4fadbffa15
ASingleCabbage <daniellangamma@gmail.com>
2016-10-15 00:57:55 -0400

    AJAX is working
    awesome

--- scripts/script.js
+++ scripts/script.js
@@ -4,44 +4,51 @@
 //    $("#test").load("http://api.tripadvisor.com/api/partner/2.0/map/"+ location + "?key=89DE2CFC0C1C43978B484B55F9A514EC");  
 //});
 
-function Poi(locId, name, rate, rank, cat, subcat, price, cuisine, dis){
-    var location_id,
-    var name,
-    var ranking_data,
-    var category,
-    var subcategory,
-    var price_level,
-    var cuisine,
-    var distance
-};
+function Poi(locId, name, rank, cat, subcat, price, cuisine, dis){
+    this.location_id = locId,
+    this.name = name,
+    this.ranking_data = rank,
+    //attraction, restaurant, hotel
+    this.category = cat,
+    //pizza, sushi, japanese, nightlife
+    this.subcategory = subcat,
+    //portrayed as '$' 
+    this.price_level = price,
+//    this.cuisine = cuisine,
+    this.distance = dis
+}
 
 $(document).on("click", "#submit", function(){
     var location = $("#textbox").val();
     $.ajax({
-        url: "http://api.tripadvisor.com/api/partner/2.0/map/"+ location + "?key=89DE2CFC0C1C43978B484B55F9A514EC", 
+        url: "http://api.tripadvisor.com/api/partner/2.0/map/"+ location + "/attractions?key=89DE2CFC0C1C43978B484B55F9A514EC", 
         dataType: "json", 
         success: function(poiData){
-            console.log(poiData);
             var poiAry = [];
-            console.log(poiData[0].name);
-//            for(var i = 0; i < poiData.length; i++){
-//                var tempPoi = poiData[i];
-//                poiAry[].push(new Poi(
-//                    tempPoi.location_id, 
-//                    tempPoi.name, 
-//                    tempPoi.rating, 
-//                    tempPoi.num_reviews, 
-//                    tempPoi.category, 
-//                    tempPoi.subcategory, 
-//                    tempPoi.price_level, 
-//                    tempPoi.cuisine, 
-//                    tempPoi.distance
-//                ));
-//             }
-        }
-            //start();     
+            for(var i = 0; i < poiData.data.length; i++){
+                var tempPoi = poiData.data[i];
+                console.log(tempPoi);
+                poiAry.push(new Poi(
+                    tempPoi.location_id, 
+                    tempPoi.name, 
+                    tempPoi.ranking_data, 
+                    tempPoi.category.name, 
+                    tempPoi.subcategory, 
+                    tempPoi.price_level, 
+//                  tempPoi.cuisine, 
+                    tempPoi.distance
+                ));
+                console.log(poiAry[i]);
+            }
+        weighPoi();
+        }     
     });
 });
+
+function weighPoi(){
+    
+
+}
 
 
 
