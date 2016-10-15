c0ad4bdb5cc21938266aad3a0c4a20d8367f2e65
ASingleCabbage <daniellangamma@gmail.com>
2016-10-14 22:57:42 -0400

    Just a prototype

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-$("#test").load("http://api.tripadvisor.com/api/partner/2.0/search/freedom%20trail%20run?key=89DE2CFC0C1C43978B484B55F9A514EC");
+$(document).on("click", "#submit", function(){
+    console.log("Hello");
+    var location = $("#textbox").val();
+    $("#test").load("http://api.tripadvisor.com/api/partner/2.0/map/"+ location + "?key=89DE2CFC0C1C43978B484B55F9A514EC");  
+});
