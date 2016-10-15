02c0ce371b1cc6e9070af76edfb1efe67cb8126c
JoeHowarth <josephehowarth@gmail.com>
2016-10-15 05:22:26 -0400

    Update input.js

--- scripts/input.js
+++ scripts/input.js
@@ -11,7 +11,8 @@
     function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 8,
-          center: {lat: -34.397, lng: 150.644} //change to tufts 
+          center: {lat: -34.397, lng: 150.644}, //change to tufts
+         mapTypeId: google.maps.MapTypeId.ROADMAP
         });
         var geocoder = new google.maps.Geocoder();
         console.log(geocoder);
