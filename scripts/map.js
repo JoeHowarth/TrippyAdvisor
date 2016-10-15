b507c51b8b04c7d62b23bea1aec9303353777283
ASingleCabbage <daniellangamma@gmail.com>
2016-10-15 06:10:17 -0400

    Added Geoplugin API for getting geoIP

---
+++ scripts/map.js
@@ -0,0 +1,13 @@
+//function initialize(){
+//    var mapProp = {
+//        center:new google.maps.LatLng(geoplugin_latitude, geoplugin_longitude),
+//        zoom: 17,
+//        mapTypeId:google.maps.MapTypeId.ROADMAP,
+//        maxZoom: 17,
+//        scrollwheel: false
+//    }
+//    
+//    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+//    google.maps.event.addDomListener(window, 'load', initialize);
+//}
+//
