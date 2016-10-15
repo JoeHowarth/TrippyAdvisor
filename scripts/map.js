5e001bf3124fd9355643ea985ff0f88c126bf706
ASingleCabbage <daniellangamma@gmail.com>
2016-10-15 12:07:11 -0400

    POI basic functions complete

--- scripts/map.js
+++ scripts/map.js
@@ -1,18 +1,3 @@
-//function initialize(){
-//    var mapProp = {
-//        center:new google.maps.LatLng(geoplugin_latitude, geoplugin_longitude),
-//        zoom: 17,
-//        mapTypeId:google.maps.MapTypeId.ROADMAP,
-//        maxZoom: 17,
-//        scrollwheel: false
-//    }
-//    
-//    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-//    google.maps.event.addDomListener(window, 'load', initialize);
-//}
-//
-
-//console.log(geoplugin_latitude());
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
@@ -29,16 +14,24 @@ function initMap() {
 }
 
 function requestDirection(tripPois){
-    console.log(tripPois[1]);
+    var waypoints = [];
+    if(tripPois.length > 2){
+        for(var i = 1; i < tripPois.length-1; i++){
+            waypoints.push({
+                location: new google.maps.LatLng(tripPois[i].lat, tripPois[i].lng),
+                stopover: true
+            });
+        }
+    }
     var request = {
         origin : new google.maps.LatLng(tripPois[0].lat, tripPois[0].lng),
         destination : new google.maps.LatLng(tripPois[tripPois.length-1].lat, tripPois[tripPois.length-1].lng),
-        travelMode : "WALKING"
+        waypoints: waypoints,
+        travelMode : "WALKING",
+        optimizeWaypoints : true
     };
-    console.log("output");
     directionsService.route(request, function(result, status) {
         if (status == 'OK') {
-            console.log("output");
             directionsDisplay.setDirections(result);
         }
     });
