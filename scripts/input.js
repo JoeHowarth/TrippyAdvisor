7adff6051736cb97dfc5da8dc6ca62d4c615fcbd
JoeHowarth <josephehowarth@gmail.com>
2016-10-15 02:51:33 -0400

    maybe working address thing

--- scripts/input.js
+++ scripts/input.js
@@ -1,2 +1,26 @@
-#input stuff
+//#input stuff
+
+
+function codeAddress() {
+    var address = document.getElementById("address").value;
+    var inp;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+		//map.setCenter(results[0].geometry.location);
+		//var marker = new google.maps.Marker({
+		//	map: map,
+		//	position: results[0].geometry.location
+		    });
+
+
+
+		 inp=results[0].geometry.location;
+	         
+		alert(inp);
+	    } else {
+		alert("Geocode was not successful for the following reason: " + status)
+		    }
+    return inp:
+
+
 
